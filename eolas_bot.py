7da934090bd37cfaba28af86eec46a43d4694302
Thomas Mahieux <thomas.mahieux@gmail.com>
2017-11-16 16:09:32 +0100

    Update eolas_bot.py

--- eolas_bot.py
+++ eolas_bot.py
@@ -2,6 +2,7 @@ import discord
 import random
 import aiohttp
 import bs4
+import requests
 from discord.ext import commands
 
 client = discord.Client()
@@ -10,9 +11,10 @@ description = '''There are a number of utility commands being showcased here.'''
 eolas = commands.Bot(command_prefix='?', description=description)
 
 hello_list = ['Yo', 'yo', 'Hey', 'hey', 'Salut', 'salut', 'Bonjour', 'bonjour']
-foot_list = ['Neymar', 'neymar','Cavani', 'cavani', 'PSG', 'psg', 'foot',
-             'football', 'Zidane', 'zidane', 'Real', 'real', 'Madrid', 'madrid'
-             'Barca', 'barca', 'OM', 'Deschamps', 'deschamps', 'Blaise']
+foot_list = ['Neymar', 'neymar','Cavani', 'cavani', 'PSG', 'psg',
+             'Zidane', 'zidane', 'Real', 'real', 'Madrid', 'madrid'
+             'Barca', 'barca', 'OM', 'Deschamps', 'deschamps', 'Blaise',
+             'blaise']
 
 # choices = (
 #     'https://media.giphy.com/media/hBO3iUfEtI2s0/giphy.gif'
@@ -36,7 +38,7 @@ async def on_ready():
     print(eolas.user.id)
     print('------')
 
-# TODO : Randomize multiple gifs for the replies triggered by foot_list
+
 @eolas.event
 async def on_message(message):
     if message.content in hello_list:
@@ -45,6 +47,31 @@ async def on_message(message):
         await eolas.send_message(message.channel, 'https://media.giphy.com/media/hBO3iUfEtI2s0/giphy.gif')
         await eolas.send_message(message.channel, "Non, pas de ça ici, s'il-vous-plaît.")
     await eolas.process_commands(message)
+
+
+# ?news - Scrape a specific block on lemonde.fr and return the news from it.
+@eolas.command()
+async def news():
+    source = requests.get('http://www.lemonde.fr/').text
+    soup = bs4.BeautifulSoup(source, 'lxml')
+    bloc = soup.find('ul', class_='liste_horaire')
+    for news_lm in bloc.find_all('li'):
+
+        hours = news_lm.span.text
+        print(hours)
+
+        try:
+            links = news_lm.find('a')['href']
+            lm_link = f'https://www.lemonde.fr/{links}'
+        except Exception as e:
+            lm_link = None
+
+        print(lm_link)
+        print()
+        if lm_link is not None:
+            await eolas.say(hours + "\n" + lm_link)
+        else:
+            pass
 
 
 @eolas.command()
@@ -95,19 +122,19 @@ async def cool(ctx):
 
 
 # TODO : facts doesn't work, get on it
-@eolas.command()
-async def facts():
-    """
-    ?facts - A command that will provide a random fact.
-    """
-    async with aiohttp.ClientSession() as session:
-        async with session.get('http://www.unkno.com') as response:
-            soup = BeautifulSoup(await response.text(), "html.parser")
-            facts = soup.find('div', attrs={'id': 'content'})
+# @eolas.command()
+# async def facts():
+#     """
+#     ?facts - A command that will provide a random fact.
+#     """
+#     async with aiohttp.ClientSession() as session:
+#         async with session.get('http://www.lemonde.fr/') as response:
+#             soup = BeautifulSoup(await response.text(), 'lxml')
+#             news = soup.select('div[class="titres clearfix"]')
+#
+#             for i in news:
+#                 i = news.strip()
+#                 await eolas.send(i)
 
-            for fact in facts:
-                fact = fact.strip()
-                await eolas.send(fact)
 
-
-eolas.run('BOT_TOKEN')
+eolas.run('Mzc2MDc1MDU3MjA3NzA1NjAw.DOIGfA.bYRke2_k2eMz7p9UU_kOsN9Ea7Y')
