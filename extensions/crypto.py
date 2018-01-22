2d7dd70813255b904abfb6d40367baf32b776a32
Thomas Mahieux <thomas.mahieux@gmail.com>
2018-01-22 14:53:28 +0100

    Update crypto.py
    Added ?coinschanges command base on my cryptomarket_scraper

--- extensions/crypto.py
+++ extensions/crypto.py
@@ -1,4 +1,8 @@
 import requests
+import pandas as pd
+import matplotlib.pyplot as plt
+from pandas.tools.plotting import table
+import discord
 from discord.ext import commands as eolas
 
 
@@ -27,6 +31,61 @@ class Crypto:
                 await eolas.say(rank + ' | ' + name.upper() + ' | ' + '$' + price_usd
                                 + ' | ' + '$' + market_cap_usd + ' | ' + change_24h + '%'
                                 + '\n' + '_ _') # '_ _' is to create a clean line break on Discord
+        
+        # ?coinschanges - Print the top 5 changes in the last 24 hrs
+        @eolas.command()
+        async def coinschanges():
+            """Print the top 5 increase (in %) in the last 24 hours."""
+            url = 'https://api.coinmarketcap.com/v1/ticker/'
+            data = requests.get(url).json()
+            channel = discord.Object(id='ID_OF_YOUR_CHANNEL')
+            ordered_data = sorted(data, key=lambda k: (float(k['percent_change_24h']),
+                                                       (k['percent_change_7d'])), reverse=True)[:5]
+            raw_data_increase = {}
+
+            for currency in ordered_data:
+                rank = currency['rank']
+                name = currency['name']
+                percent_change_24h = currency['percent_change_24h']
+                percent_change_7d = currency['percent_change_7d']
+                price_usd = float(currency['price_usd'])
+
+                raw_data_increase.setdefault('Rank', [])
+                raw_data_increase['Rank'].append(rank)
+                raw_data_increase.setdefault('Name', [])
+                raw_data_increase['Name'].append(name)
+                raw_data_increase.setdefault('Change last 24h (%)', [])
+                raw_data_increase['Change last 24h (%)'].append(percent_change_24h)
+                raw_data_increase.setdefault('Change last 7d (%)', [])
+                raw_data_increase['Change last 7d (%)'].append(percent_change_7d)
+                raw_data_increase.setdefault('Price(USD)', [])
+                raw_data_increase['Price(USD)'].append(price_usd)
+                # print(raw_data)
+
+            df = pd.DataFrame(raw_data_increase,
+                              columns=['Rank', 'Name', 'Change last 24h (%)',
+                                       'Change last 7d (%)', 'Price(USD)'])
+
+            print(df.to_string(index=False))
+
+            # set fig size
+            fig, ax = plt.subplots(figsize=(9.5, 2.7))
+            # no axes
+            ax.xaxis.set_visible(False)
+            ax.yaxis.set_visible(False)
+            # no frame
+            ax.set_frame_on(False)
+            # plot table
+            tab = table(ax, df, rowLabels=[''] * df.shape[0], loc='center')
+            # set font manually
+            tab.auto_set_font_size(False)
+            tab.set_fontsize(10)
+            # set scale
+            tab.scale(1, 2.5)
+            # save the result
+            plt.savefig('cryptopinc.png')
+
+            await eolas.send_file(channel, 'cryptopinc.png')
 
 
 def setup(eolas):
