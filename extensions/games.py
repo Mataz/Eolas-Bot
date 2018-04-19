2563702ceaf788b1230a7c8d4d058385d15ce1d7
Thomas Mahieux <thomas.mahieux@gmail.com>
2018-04-19 15:08:11 +0200

    Update games.py
    Added ?hearthstone command

--- extensions/games.py
+++ extensions/games.py
@@ -2,6 +2,7 @@ import random
 import discord
 import bs4
 import requests
+from selenium import webdriver
 from discord.ext import commands as eolas
 
 
@@ -18,7 +19,36 @@ class Games:
             puzzle_link = f'https://lichess.org/training/{("".join(map(str, random_number)))}'
             print('Lichess Puzzle ID:' + '\n' + random_ID + '\n')
             await eolas.say('Lichess Puzzle:' + '\n' + puzzle_link)
-            
+        
+        # ?hearthstone - Print the current top 3 overperforming decks listed on hsreplay
+        @eolas.command()
+        async def hearthstone():
+            """Print the current top 3 overperforming decks listed on hsreplay"""
+            driver = webdriver.Chrome()
+            driver.implicitly_wait(15)
+            driver.get('https://hsreplay.net/meta/')
+
+            tier_elem = driver.find_elements_by_class_name('tier-header')
+            tiers = [x.text for x in tier_elem][:1]
+
+            names_elem = driver.find_elements_by_class_name('archetype-name')
+            names = [x.text for x in names_elem][:3]
+
+            percent_elem = driver.find_elements_by_class_name('archetype-data')
+            percents = [x.text for x in percent_elem][:3]
+
+            links = [link.get_attribute('href') for link in driver.find_elements_by_xpath(
+                '//*[@id="tierlist"]/div/div/div/li/a/div/a')][:3]
+
+            for tier in tiers:
+                await eolas.say(str(tier[6:])+ '\n' + '--------------------'
+                                                      '----------------------------')
+            for name, percent, link in zip(names, percents, links):
+                await eolas.say(str(name) + ' > ' + str(percent) + ' winrate ' + ' | ' + str(
+                    link) + '\n' + '------------------------------------------------')
+            driver.quit()
+
+
         # ?gwent - Print the top 5 decks of the week on gwentdb.com
         @eolas.command()
         async def gwent():
